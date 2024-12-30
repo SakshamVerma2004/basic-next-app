5763fa9924e2744c670847b28cfed169fc1e4dd3
unknown <sakshamverma798@gmail.com>
2024-12-30 17:16:16 +0530

    made the dynamic routes pages statc too

--- app/dynamic-routes/[id]/page.js
+++ app/dynamic-routes/[id]/page.js
@@ -1,29 +1,21 @@
-import { notFound } from "next/navigation";
-
 export async function generateStaticParams() {
-  // Generate two pages at build time and the rest (3-100) on-demand
-  return [{ id: "1" }, { id: "2" }];
+  let fetchUser = await fetch("https://jsonplaceholder.typicode.com/users");
+  let res = await fetchUser.json();
+
+  return res.map((user) => ({
+    id: user.id.toString(),
+  }));
 }
 
-export default async function Page(props) {
-  const params = await props.params;
-  if (Number(params.id) >= 100) {
-    notFound();
+let Page = async ({ params }) => {
+  let fetchUser = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.id}`
+  );
+  let res = await fetchUser.json();
+  if (!res || !res.id) {
+    return <div>User not found</div>;
   }
+  return <div>User name: {res.name}</div>;
+};
 
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id}`
-  );
-  const data = await res.json();
-
-  return (
-    <div className="grid grid-cols-6 gap-x-6 gap-y-3">
-      <div className="col-span-full space-y-3 lg:col-span-4">
-        <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
-          {data.title}
-        </h1>
-        <p className="line-clamp-3 font-medium text-gray-500">{data.body}</p>
-      </div>
-    </div>
-  );
-}
+export default Page;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
+"use client"
+
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 let Home = () => {
+  useEffect(() => {
+    fetch("https://brightlight-node.onrender.com/adding-blog")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },[]);
   return (
       <div className={styles.page}>
         <h1>This is the homepage.</h1>
